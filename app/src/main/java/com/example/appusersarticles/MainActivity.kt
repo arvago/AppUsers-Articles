69ebe5f4db191dea6949f37f7e3e1d2035272d80
Arturo Vallejo González <71914027+arvago@users.noreply.github.com>
2021-09-14 18:13:36 -0500

    MainActivity

--- app/src/main/java/com/example/appusersarticles/MainActivity.kt
+++ app/src/main/java/com/example/appusersarticles/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.appusersarticles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.container, LogInFragment()).commit()
+    }
+
+    fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(
+                R.anim.slide_in_right,
+                R.anim.slide_out_left,
+                R.anim.slide_in_left,
+                R.anim.slide_out_right)
+            replace(R.id.container, fragment)
+            addToBackStack(fragment.tag)
+            commit()
+        }
     }
 }
