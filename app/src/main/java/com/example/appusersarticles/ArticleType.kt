5406c2a0a0fec3e6447ef4f8e5f133fe6d5e4787
Arturo Vallejo González <71914027+arvago@users.noreply.github.com>
2021-09-15 00:10:21 -0500

    Class Article

---
+++ app/src/main/java/com/example/appusersarticles/ArticleType.kt
@@ -0,0 +1,9 @@
+package com.example.appusersarticles
+
+enum class ArticleType(val image: Int){
+    DEPORTE(R.drawable.Deporte),
+    SALUD(R.drawable.Salud),
+    SOCIAL(R.drawable.Social),
+    ENTRETENIMIENTO(R.drawable.Entretenimiento),
+    POLITICA(R.drawable.Politica)
+}
